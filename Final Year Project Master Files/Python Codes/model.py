f3fdaa96f892065db49e2b34904bb7d4dfe0e19e
NigelKok <nigel-kok@hotmail.com>
2019-01-10 21:41:34 +0800

    Arranged files for 2D cases, old files are in OpenFOAM_UnusedFiles

--- Final Year Project Master Files/Python Codes/model.py
+++ Final Year Project Master Files/Python Codes/model.py
@@ -36,18 +36,20 @@ def encoderDecoder():
     cnn.add(Conv2DTranspose(256, (4,8), padding='same', activation='relu'))
     cnn.add(Conv2DTranspose(32, (2,2), padding='same', activation='relu'))
     cnn.add(Conv2DTranspose(3, (2,2), padding='same', activation='relu'))
-    
+
     #cnn.add(MaxPooling2D(pool_size=(2, 2)))
     #cnn.add(Flatten())
 
     cnn.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
-    
+
     #history = LossHistory()
-    cnnModel = cnn.fit(X_array3U_negGeometry[:14], Y_array3U[:14], validation_split=0.3, batch_size = 1, epochs = 2)
+    cnnModel = cnn.fit(X_array3U_negGeometry[:14], Y_array3U[:14], validation_split=0.3, batch_size = 1, epochs = 10)
+
+
+    y_predict = cnn.predict(np.reshape(X_array3U_negGeometry[14], (1, 128, 256, 3)))
+    print(y_predict)
     plotLoss(cnnModel)
 
-    y_predict = cnn.predict(np.reshape(X_array3U[14], (1, 128, 256, 3)))
-    
 # For trial purposes
 encoderDecoder()
-# y_predict = cnn.predict(np.reshape(X_array3U[14],(1,128,256,3)))
+#y_predict = cnn.predict(np.reshape(X_array3U[14],(1,128,256,3)))
