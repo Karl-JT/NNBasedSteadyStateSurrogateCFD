fab30ab462a37d63cd9b3198e645b0c4c88fc84b
Nigel <nigel-kok@hotmail.com>
2019-03-26 16:10:28 +0800

    Modified model to exclude pressure field

--- FYP_MasterFilesNEW/PythonCodes/excelToNumpy.py
+++ FYP_MasterFilesNEW/PythonCodes/excelToNumpy.py
@@ -10,15 +10,15 @@ wdir = os.chdir(os.getcwd()+'/LinkedFolderNEW')
 trainingCases = os.listdir(os.getcwd())
 trainingCasesListLen = len(trainingCases)
 
-#X_array = np.zeros((trainingCasesListLen, 128, 256))
+X_array = np.zeros((trainingCasesListLen, 128, 256))
 Y_array = np.zeros((trainingCasesListLen, 128, 256))
-#X_array3U = np.zeros((trainingCasesListLen, 128, 256, 3))
-#Y_array3U = np.zeros((trainingCasesListLen, 128, 256, 3))
+X_array3U = np.zeros((trainingCasesListLen, 128, 256, 3))
+Y_array3U = np.zeros((trainingCasesListLen, 128, 256, 3))
 
-X_arrayXVelWPressure = np.zeros((trainingCasesListLen, 128, 256, 2))
-Y_arrayXVelWPressure = np.zeros((trainingCasesListLen, 128, 256, 2))
-X_arrayYVelWPressure = np.zeros((trainingCasesListLen, 128, 256, 2))
-Y_arrayYVelWPressure = np.zeros((trainingCasesListLen, 128, 256, 2))
+#X_arrayXVelWPressure = np.zeros((trainingCasesListLen, 128, 256, 2))
+#Y_arrayXVelWPressure = np.zeros((trainingCasesListLen, 128, 256, 2))
+#X_arrayYVelWPressure = np.zeros((trainingCasesListLen, 128, 256, 2))
+#Y_arrayYVelWPressure = np.zeros((trainingCasesListLen, 128, 256, 2))
 
 for count in range(trainingCasesListLen):
     # Open final iteration to get output array
@@ -28,16 +28,16 @@ for count in range(trainingCasesListLen):
 
     for x in range(len(rawData)):
         tempLine = rawData.iloc[x]
-        #Y_array3U[count, int(tempLine[1]*10),int(tempLine[0]*10)] = (tempLine[3],tempLine[4],tempLine[5])
+        Y_array3U[count, int(tempLine[1]*10),int(tempLine[0]*10)] = (tempLine[3],tempLine[4],tempLine[5])
         Y_array[count, int(tempLine[1]*10),int(tempLine[0]*10)] = tempLine[3]
-        Y_arrayXVelWPressure[count, int(tempLine[1]*10),int(tempLine[0]*10)] = (tempLine[3], 0)
-        Y_arrayYVelWPressure[count, int(tempLine[1]*10),int(tempLine[0]*10)] = (tempLine[4], 0)
+        #Y_arrayXVelWPressure[count, int(tempLine[1]*10),int(tempLine[0]*10)] = (tempLine[3], 0)
+        #Y_arrayYVelWPressure[count, int(tempLine[1]*10),int(tempLine[0]*10)] = (tempLine[4], 0)
     
-    rawData = pd.read_csv('data_p.csv')
-    for x in range(len(rawData)):
-        tempLine = rawData.iloc[x]
-        Y_arrayXVelWPressure[count, int(tempLine[1]*10),int(tempLine[0]*10),1] = (tempLine[3])
-        Y_arrayYVelWPressure[count, int(tempLine[1]*10),int(tempLine[0]*10),1] = (tempLine[3])
+    #rawData = pd.read_csv('data_p.csv')
+    #for x in range(len(rawData)):
+    #    tempLine = rawData.iloc[x]
+    #    Y_arrayXVelWPressure[count, int(tempLine[1]*10),int(tempLine[0]*10),1] = (tempLine[3])
+    #    Y_arrayYVelWPressure[count, int(tempLine[1]*10),int(tempLine[0]*10),1] = (tempLine[3])
         
     # Open first iteration to get input array
     os.chdir('..')
@@ -46,16 +46,16 @@ for count in range(trainingCasesListLen):
     
     for x in range(len(rawData2)):
         tempLine = rawData2.iloc[x]
-        #X_array3U[count, int(tempLine[1]*10),int(tempLine[0]*10)] = (tempLine[3],tempLine[4],tempLine[5])
-        #X_array[count, int(tempLine[1]*10),int(tempLine[0]*10)] = tempLine[3]    
-        X_arrayXVelWPressure[count, int(tempLine[1]*10),int(tempLine[0]*10)] = (tempLine[3], 0)
-        X_arrayYVelWPressure[count, int(tempLine[1]*10),int(tempLine[0]*10)] = (tempLine[4], 0)
+        X_array3U[count, int(tempLine[1]*10),int(tempLine[0]*10)] = (tempLine[3],tempLine[4],tempLine[5])
+        X_array[count, int(tempLine[1]*10),int(tempLine[0]*10)] = tempLine[3]    
+        #X_arrayXVelWPressure[count, int(tempLine[1]*10),int(tempLine[0]*10)] = (tempLine[3], 0)
+        #X_arrayYVelWPressure[count, int(tempLine[1]*10),int(tempLine[0]*10)] = (tempLine[4], 0)
 
-    rawData = pd.read_csv('data_p.csv')
-    for x in range(len(rawData)):
-        tempLine = rawData.iloc[x]
-        X_arrayXVelWPressure[count, int(tempLine[1]*10),int(tempLine[0]*10),1] = (tempLine[3])
-        X_arrayYVelWPressure[count, int(tempLine[1]*10),int(tempLine[0]*10),1] = (tempLine[3])
+    #rawData = pd.read_csv('data_p.csv')
+    #for x in range(len(rawData)):
+    #    tempLine = rawData.iloc[x]
+    #    X_arrayXVelWPressure[count, int(tempLine[1]*10),int(tempLine[0]*10),1] = (tempLine[3])
+    #    X_arrayYVelWPressure[count, int(tempLine[1]*10),int(tempLine[0]*10),1] = (tempLine[3])
         
     os.chdir('..')
     os.chdir('..')
@@ -64,27 +64,29 @@ for count in range(trainingCasesListLen):
 
 
 # Create new Y_array and X_array with geometry area as -1
-#X_array_negGeometry = X_array
+X_array_negGeometry = X_array
 Y_array_negGeometry = Y_array
-#X_array3U_negGeometry = X_array3U
-#Y_array3U_negGeometry = Y_array3U
-X_arrayXVelWPressure_negGeometry = X_arrayXVelWPressure
-X_arrayYVelWPressure_negGeometry = X_arrayYVelWPressure
-Y_arrayXVelWPressure_negGeometry = Y_arrayXVelWPressure
-Y_arrayYVelWPressure_negGeometry = Y_arrayYVelWPressure
+X_array3U_negGeometry = X_array3U
+Y_array3U_negGeometry = Y_array3U
+
+#X_arrayXVelWPressure_negGeometry = X_arrayXVelWPressure
+#X_arrayYVelWPressure_negGeometry = X_arrayYVelWPressure
+#Y_arrayXVelWPressure_negGeometry = Y_arrayXVelWPressure
+#Y_arrayYVelWPressure_negGeometry = Y_arrayYVelWPressure
 
 for count in range(len(Y_array)):
     for x in range(256):
         for y in range(128):
             if Y_array[count, y, x] == 0:
-                #Y_array_negGeometry[count, y, x] = -1
-                #X_array_negGeometry[count, y, x] = -1
-                #Y_array3U_negGeometry[count, y, x] = (-1, -1, -1)
-                #X_array3U_negGeometry[count, y, x] = (-1, -1, -1)
-                X_arrayXVelWPressure_negGeometry[count, y, x] = (-1, -1)
-                X_arrayYVelWPressure_negGeometry[count, y, x] = (-1, -1)
-                Y_arrayXVelWPressure_negGeometry[count, y, x] = (-1, -1)
-                Y_arrayYVelWPressure_negGeometry[count, y, x] = (-1, -1)
+                Y_array_negGeometry[count, y, x] = -1
+                X_array_negGeometry[count, y, x] = -1
+                Y_array3U_negGeometry[count, y, x] = (-1, -1, -1)
+                X_array3U_negGeometry[count, y, x] = (-1, -1, -1)
+                
+                #X_arrayXVelWPressure_negGeometry[count, y, x] = (-1, -1)
+                #X_arrayYVelWPressure_negGeometry[count, y, x] = (-1, -1)
+                #Y_arrayXVelWPressure_negGeometry[count, y, x] = (-1, -1)
+                #Y_arrayYVelWPressure_negGeometry[count, y, x] = (-1, -1)
 
 # Reset to Python Codes directory
 os.chdir('..')
--- FYP_MasterFilesNEW/PythonCodes/model.py
+++ FYP_MasterFilesNEW/PythonCodes/model.py
@@ -8,10 +8,13 @@ from keras.callbacks import History
 from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 from keras.preprocessing import image
-#from excelToNumpy import X_array, Y_array, X_array3U, Y_array3U, X_array_negGeometry, Y_array_negGeometry, X_array3U_negGeometry, Y_array3U_negGeometry
-from excelToNumpy import X_arrayXVelWPressure_negGeometry, Y_arrayXVelWPressure_negGeometry, X_arrayYVelWPressure_negGeometry, Y_arrayYVelWPressure_negGeometry
-#from airfoilShapeExtractor import X_airfoil, Y_airfoil, X_airfoil3U, Y_airfoil3U, X_airfoil_negGeometry, Y_airfoil_negGeometry, X_airfoil3U_negGeometry, Y_airfoil3U_negGeometry
-from airfoilShapeExtractor import X_airfoilXVelWPressure_negGeometry, X_airfoilYVelWPressure_negGeometry, Y_airfoilXVelWPressure_negGeometry, Y_airfoilYVelWPressure_negGeometry
+
+from excelToNumpy import X_array, Y_array, X_array3U, Y_array3U, X_array_negGeometry, Y_array_negGeometry, X_array3U_negGeometry, Y_array3U_negGeometry
+#from excelToNumpy import X_arrayXVelWPressure_negGeometry, Y_arrayXVelWPressure_negGeometry, X_arrayYVelWPressure_negGeometry, Y_arrayYVelWPressure_negGeometry
+
+from airfoilShapeExtractor import X_airfoil, Y_airfoil, X_airfoil3U, Y_airfoil3U, X_airfoil_negGeometry, Y_airfoil_negGeometry, X_airfoil3U_negGeometry, Y_airfoil3U_negGeometry
+#from airfoilShapeExtractor import X_airfoilXVelWPressure_negGeometry, X_airfoilYVelWPressure_negGeometry, Y_airfoilXVelWPressure_negGeometry, Y_airfoilYVelWPressure_negGeometry
+
 from helperFunctions import *
 
 
@@ -26,13 +29,13 @@ class LossHistory(keras.callbacks.Callback):
 def encoderDecoderXVelocity():
     name = 'EncoderDecoderXVel'
     cnn = Sequential()
-    cnn.add(Conv2D(128, (8,16), padding='same', input_shape=(128, 256, 2), activation='relu'))
+    cnn.add(Conv2D(128, (8,16), padding='same', input_shape=(128, 256, 1), activation='relu'))
     cnn.add(Conv2D(512, (4,4), padding='same', activation='relu'))
 
     cnn.add(Conv2DTranspose(512, (8,8), padding='same', activation='relu'))
     cnn.add(Conv2DTranspose(256, (4,8), padding='same', activation='relu'))
     cnn.add(Conv2DTranspose(32, (2,2), padding='same', activation='relu'))
-    cnn.add(Conv2DTranspose(2, (2,2), padding='same', activation='relu'))
+    cnn.add(Conv2DTranspose(1, (2,2), padding='same', activation='relu'))
 
     #cnn.add(MaxPooling2D(pool_size=(2, 2))) 
     #cnn.add(Flatten())
@@ -45,7 +48,10 @@ def encoderDecoderXVelocity():
 
     #history = LossHistory()
     if checkSaveExist(name) == 0:
-        cnnModel = cnn.fit(X_arrayXVelWPressure_negGeometry[:], Y_arrayXVelWPressure_negGeometry[:], validation_split=0.2, batch_size = 1, epochs = 10)
+        len1 = len(X_array3U_negGeometry)
+        input1 = np.reshape(X_array3U_negGeometry[:,:,:,0], (len1, 128, 256, 1))
+        output1 = np.reshape(Y_array3U_negGeometry[:,:,:,0], (len1, 128, 256, 1))
+        cnnModel = cnn.fit(input1, output1, validation_split=0.2, batch_size = 1, epochs = 10)
         # Serialize model to JSON
         model_json = cnn.to_json()
         weightsFile = name + '.h5'
@@ -56,7 +62,7 @@ def encoderDecoderXVelocity():
         cnn.save_weights(weightsFile)
         print("Saved model to disk as {}.".format(weightsFile))
 
-    y_predictXVel = cnn.predict(np.reshape(X_airfoilXVelWPressure_negGeometry, (1, 128, 256, 2)))
+    y_predictXVel = cnn.predict(np.reshape(X_airfoil3U_negGeometry[1,:,:,0], (1, 128, 256, 1)))
     print('X velocity of predicted array is {}'.format(y_predictXVel[:,:,:,0]))
     
     # Plot X velocity
@@ -71,13 +77,13 @@ def encoderDecoderXVelocity():
 def encoderDecoderYVelocity():
     name = 'EncoderDecoderYVel'
     cnn = Sequential()
-    cnn.add(Conv2D(128, (8,16), padding='same', input_shape=(128, 256, 2), activation='relu'))
+    cnn.add(Conv2D(128, (8,16), padding='same', input_shape=(128, 256, 1), activation='relu'))
     cnn.add(Conv2D(512, (4,4), padding='same', activation='relu'))
 
     cnn.add(Conv2DTranspose(512, (8,8), padding='same', activation='relu'))
     cnn.add(Conv2DTranspose(256, (4,8), padding='same', activation='relu'))
     cnn.add(Conv2DTranspose(32, (2,2), padding='same', activation='relu'))
-    cnn.add(Conv2DTranspose(2, (2,2), padding='same', activation='relu'))
+    cnn.add(Conv2DTranspose(1, (2,2), padding='same', activation='relu'))
 
     #cnn.add(MaxPooling2D(pool_size=(2, 2))) 
     #cnn.add(Flatten())
@@ -90,7 +96,10 @@ def encoderDecoderYVelocity():
 
     #history = LossHistory()
     if checkSaveExist(name) == 0:
-        cnnModel = cnn.fit(X_arrayYVelWPressure_negGeometry[:], Y_arrayYVelWPressure_negGeometry[:], validation_split=0.2, batch_size = 1, epochs = 10)
+        len2 = len(X_array3U_negGeometry)
+        input2 = np.reshape(X_array3U_negGeometry[:,:,:,1], (len2, 128, 256, 1) )
+        output2 = np.reshape(Y_array3U_negGeometry[:,:,:,1], (len2, 128, 256, 1))
+        cnnModel = cnn.fit(input2, output2, validation_split=0.2, batch_size = 1, epochs = 10)
         # Serialize model to JSON
         model_json = cnn.to_json()
         weightsFile = name + '.h5'
@@ -101,12 +110,12 @@ def encoderDecoderYVelocity():
         cnn.save_weights(weightsFile)
         print("Saved model to disk as {}.".format(weightsFile))
 
-    y_predictYVel = cnn.predict(np.reshape(X_airfoilYVelWPressure_negGeometry, (1, 128, 256, 2)))
+    y_predictYVel = cnn.predict(np.reshape(X_airfoil3U_negGeometry[1,:,:,1], (1, 128, 256, 1)))
     print('Y velocity of predicted array is {}'.format(y_predictYVel[:,:,:,0]))
        
     # Plot Y velocity
     tempName = name + 'Yvelocity'
-    plotArray(y_predictYVel[0,:,:,1], tempName)
+    plotArray(y_predictYVel[0,:,:,0], tempName)
     
     plotLoss(cnnModel, "encoderDecoderYVelocityLoss")
 
