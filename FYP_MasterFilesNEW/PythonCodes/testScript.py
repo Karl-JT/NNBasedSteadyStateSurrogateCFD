d3c7276b299bcccb00bfd8b9c3bd8d89c98b4359
Nigel <nigel-kok@hotmail.com>
2019-04-03 18:55:50 +0800

    Added dense layer

--- FYP_MasterFilesNEW/PythonCodes/testScript.py
+++ FYP_MasterFilesNEW/PythonCodes/testScript.py
@@ -12,25 +12,27 @@ import numpy as np
 import os
 from PIL import Image
 
-def plotArray(array):
+def plotArray(array, name):
     # Takes in array and displays array as image with colourbar
     img = plt.imshow(array, interpolation='nearest')
     plt.colorbar(img, shrink = 0.62)
+    
     plt.axis([0,256, 0, 128])
     plt.ylabel('Y')
     plt.xlabel('X')
-    saveAsJPG('Contour')
+    imageName = name + 'Contour'
+    saveAsPNG(imageName)
     plt.show()
     return
 
-def saveAsJPG(name):
-    # Saves plot as JPEG file
-    os.chdir(os.getcwd()+'/plotImages')
+def saveAsPNG(name):
+    # Saves plot as PNG file
     numImages = len(os.listdir(os.getcwd()))
     idx = numImages + 1
     plt.savefig(name+str(idx)+'.png')
-    os.chdir('..')
     return
 
-xx = Y_array[0]
-plotArray(xx)
+testX = Y_airfoil3U[:,:,:,0]
+testY = Y_airfoil3U[:,:,:,1]
+plotArray(testX[0,:,:], 'testX')
+plotArray(testY[0,:,:], 'testY')
